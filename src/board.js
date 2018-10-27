de10bd0044dfe171fd27b4ad942ce531196c57fd
Mightynumnums <deadcookies@gmail.com>
2018-10-27 13:38:35 -0400

    storing history of moves

--- src/board.js
+++ src/board.js
@@ -1,51 +1,21 @@
 import React from 'react'
 import './index.css'
-import Square from './square.js'
-import { calculateWinner } from './index.js'
+import Square from './Square.js'
+
 
 export default class Board extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice()
-    if (calculateWinner(squares) || squares[i]) {
-      return
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O'
-    this.setState({
-      squares,
-      xIsNext: !this.state.xIsNext
-    })
-  }
-
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     )
   }
 
-
   render() {
-    const winner = calculateWinner(this.state.squares)
-    let status
-    if (winner) {
-      status = `Winner ${winner}`
-    } else {
-      status = `Next player ${this.state.xIsNext ? 'X' : 'O'}`
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -61,7 +31,7 @@ export default class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
-      </div >
+      </div>
     )
   }
 }
